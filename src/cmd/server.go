3f380675af8e29b72f15c9c0f5a5d3dd324dfce0
Nayan Das <dastms@gmail.com>
2025-04-01 05:43:58 +0000

    Try with go max procs

--- src/cmd/server.go
+++ src/cmd/server.go
@@ -4,12 +4,18 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"runtime"
 
 	_ "net/http/pprof"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 
 		// logger := slog.With("component", "server")
 
@@ -21,8 +27,16 @@ func main() {
 		w.Write([]byte(`{ "message": "hi!" }`))
 	})
 
+	server := &http.Server{
+		Addr:    ":9000",
+		Handler: mux,
+		// MaxHeaderBytes:    1 << 20,
+		// IdleTimeout:       0, // Prevent idle connections from blocking
+		// ReadHeaderTimeout: 0, // Prevent slow readers from blocking
+	}
+
 	slog.Info("Listening on 0.0.0.0:9000")
-	if err := http.ListenAndServe("0.0.0.0:9000", http.DefaultServeMux); err != nil {
+	if err := server.ListenAndServe(); err != nil {
 		slog.Error("Error while serving server", "err", err)
 		os.Exit(1)
 	}
